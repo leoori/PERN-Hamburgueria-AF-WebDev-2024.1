fb777a3ec760dbc41f896f8b72e99e1fb0b09c2d
leoori <alemaoproenca@hotmail.com>
2024-06-19 23:30:18 -0300

    Primeiro commit

---
+++ server/src/db/index.js
@@ -0,0 +1,13 @@
+const { query, text } = require('express')
+const {Pool} = require('pg')
+const pool = new Pool({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'pern_login',
+    password: '1503',
+    port: 5432,
+})
+
+module.exports = {
+    query: (text, params) => pool.query(text, params),
+}
